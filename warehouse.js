87ce617fb13d0e4191f65ecc30bdb8c3a0e90543
Maksim Tseljabov <maksim.tseljabov@gmail.com>
2016-06-01 20:36:30 +0300

    upd

--- warehouse.js
+++ warehouse.js
@@ -7,6 +7,11 @@ document.querySelector('#login_form_open button').addEventListener('click',
     function () {
         document.getElementById('login_form').style.display = 'block';
         document.getElementById('login_form_open').style.display = 'none';
+
+
+        document.getElementById('register_form').style.display = 'none';
+        document.getElementById('register_form_open').style.display = 'block';
+
     });
 
 
@@ -25,6 +30,9 @@ document.querySelector('#register_form_open button').addEventListener('click',
     function () {
         document.getElementById('register_form').style.display = 'block';
         document.getElementById('register_form_open').style.display = 'none';
+
+        document.getElementById('login_form').style.display = 'none';
+        document.getElementById('login_form_open').style.display = 'block';
     });
 
 
